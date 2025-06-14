a7228325e096adc677e7509d9744162ad374e015
Leonardo <56612664+LeonardoSGSG@users.noreply.github.com>
2025-06-14 00:27:02 -0500

    first commit

---
+++ ms05-xml-reader/src/main/java/com/intercorp/ms05/Ms05XmlReaderApplication.java
@@ -0,0 +1,13 @@
+package com.intercorp.ms05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ms05XmlReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ms05XmlReaderApplication.class, args);
+	}
+
+}
